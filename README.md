3961707c4ca01b924dfedde0df48a563e89b17b8
scotch27 <scotch27@mail.ru>
2023-08-27 01:25:39 +0300

    Исправлено отображение promo__logo

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 Дипломный проект по курсу Веб-разработчик. Яндекс Практикум
 
 ## Макет в Figma
-Выбран макет: light-1
+Выбран макет: light-1<BR/>
 Ссылка на макет: https://www.figma.com/file/y1hL6XseST0zcmF2IBjVyK/light-1-(Copy)?type=design&node-id=891-3857&mode=design&t=s3uLHRmcSAgWWe7n-0
--- src/components/Main/Promo/Promo.js
+++ src/components/Main/Promo/Promo.js
@@ -7,15 +7,17 @@ function Promo() {
   return (
     <section className="promo">
       <div className="promo__container">
-        <h1 className="promo__title">
-          Учебный проект студента факультета Веб&#8209;разработки.
-        </h1>
-        <p className="promo__subtitle">
-          Листайте ниже, чтобы узнать больше про этот проект и его создателя.
-        </p>
-        <a className="promo__link" href="#project">
-          Узнать больше
-        </a>
+        <div className="promo__content">
+          <h1 className="promo__title">
+            Учебный проект студента факультета Веб&#8209;разработки.
+          </h1>
+          <p className="promo__subtitle">
+            Листайте ниже, чтобы узнать больше про этот проект и его создателя.
+          </p>
+          <a className="promo__link" href="#project">
+            Узнать больше
+          </a>
+        </div>
         <img className="promo__logo" src={landingLogo} alt="Логотип web" />
       </div>
     </section>
