4f1dccdf0b2de7402ed5249c1860b304eeebb32b
scotch27 <scotch27@mail.ru>
2023-08-30 15:59:50 +0300

    Настроен раздел login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,13 +1,87 @@
 // Login — компонент страницы авторизации.
 
-import './Login.css';
+import React, { useEffect } from "react";
+import CurrentUserContext from "../../contexts/CurrentUserContext";
+import { Link } from "react-router-dom";
+import AuthForm from "../AuthForm/AuthForm";
+import useForm from "../../hooks/useForm";
+import { PAGES } from "../../utils/const";
 
 function Login() {
-    return (
-        <div className="Login">
-            <h1>Login</h1>
-        </div>
-    );
+  const formName = "login";
+  const loginEmail = "email";
+  const loginPassword = "password";
+
+  const currentUser = React.useContext(CurrentUserContext);
+  const { values, errors, handleChange, isFormValid, resetForm } =
+    useForm(formName);
+
+  const footerText = (
+    <>
+      Ещё не зарегистрированы?{" "}
+      <Link className="auth-form__link" to={PAGES.REGISTER}>
+        Регистрация
+      </Link>
+    </>
+  );
+
+  const error = "При обновлении профиля произошла ошибка.";
+
+  useEffect(() => {
+    if (currentUser) {
+      resetForm(currentUser);
+    }
+  }, [currentUser, resetForm]);
+
+  function handleSubmit(e) {
+    // Запрещаем браузеру переходить по адресу формы
+    e.preventDefault();
+
+    console.log("handleSubmit Login");
+  }
+
+  return (
+    <AuthForm
+      formName={formName}
+      title="Рады видеть!"
+      buttonText="Войти"
+      footerText={footerText}
+      onSubmit={handleSubmit}
+      isFormValid={isFormValid}
+      error={error}
+    >
+      <label className="auth-form__field" htmlFor={loginEmail}>
+        E-mail
+        <input
+          name={loginEmail}
+          className="auth-form__input"
+          id={loginEmail}
+          type="email"
+          required
+          onChange={handleChange}
+          value={values[loginEmail] || ""}
+        />
+        <span className="auth-form__input-error">{errors[loginEmail]}</span>
+      </label>
+
+      <label className="auth-form__field" htmlFor={loginPassword}>
+        Пароль
+        <input
+          name={loginPassword}
+          className={`auth-form__input ${
+            errors[loginPassword] ? "auth-form__input_error" : ""
+          }`}
+          id={loginPassword}
+          type="password"
+          required
+          minLength="8"
+          onChange={handleChange}
+          value={values[loginPassword] || ""}
+        />
+        <span className="auth-form__input-error">{errors[loginPassword]}</span>
+      </label>
+    </AuthForm>
+  );
 }
 
 export default Login;
