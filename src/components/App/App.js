6d8f7d5c1d1a71dc4e7a3a4c0bd4b44fa12d39b7
scotch27 <scotch27@mail.ru>
2023-08-30 22:52:08 +0300

    Добавлен ProtectedRoute

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
+// import { Route, Switch, Routes, Navigate } from "react-router-dom";
 import CurrentUserContext from "../../contexts/CurrentUserContext";
 import "./App.css";
 import Main from "../Main/Main";
@@ -9,14 +10,19 @@ import Profile from "../Profile/Profile";
 import Login from "../Login/Login";
 import Register from "../Register/Register";
 import NotFound from "../NotFound/NotFound";
+import ProtectedRoute from "../ProtectedRoute/ProtectedRoute";
 import { PAGES } from "../../utils/const";
 
 function App() {
-  const [currentUser, setCurrentUser] = useState({name: "Виталий", email:"pochta@yandex.ru"});
-  const [loggedIn, setLoggedIn] = useState(true);
+  const [currentUser, setCurrentUser] = useState({
+    name: "Виталий",
+    email: "pochta@yandex.ru",
+  });
+  const [loggedIn, setLoggedIn] = useState(false);
 
   const signOut = () => {
-     setLoggedIn(false);
+    console.log("loggedIn!");
+    setLoggedIn(false);
   };
 
   return (
@@ -24,11 +30,24 @@ function App() {
       <div className="app">
         <Routes>
           <Route path={PAGES.MAIN} element={<Main loggedIn={loggedIn} />} />
-          <Route path={PAGES.MOVIES} element={<Movies loggedIn={loggedIn}/>} />
-          <Route path={PAGES.SAVED_MOVIES} element={<SavedMovies loggedIn={loggedIn}/>} />
-          <Route path={PAGES.PROFILE} element={<Profile loggedIn={loggedIn} signOut={signOut} />} />
           <Route path={PAGES.LOGIN} element={<Login />} />
           <Route path={PAGES.REGISTER} element={<Register />} />
+
+          <Route element={<ProtectedRoute loggedIn={loggedIn} />}>
+            <Route
+              path={PAGES.MOVIES}
+              element={<Movies loggedIn={loggedIn} />}
+            />
+            <Route
+              path={PAGES.SAVED_MOVIES}
+              element={<SavedMovies loggedIn={loggedIn} />}
+            />
+            <Route
+              path={PAGES.PROFILE}
+              element={<Profile signOut={signOut} />}
+            />
+          </Route>
+
           <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
