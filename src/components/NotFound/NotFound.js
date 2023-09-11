f0f86e1531b14fdcc81b92fed47c33d4fa0eef71
scotch27 <scotch27@mail.ru>
2023-09-11 23:23:14 +0300

    Исправлена кнопка назад в NotFound

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,18 +1,22 @@
 // NotFound — компонент ненайденной страницы
 
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./NotFound.css";
 import { PAGES, ERROR_MSG_PAGE_NOT_FOUND } from "../../utils/const";
 
 function NotFound() {
+  const navigate = useNavigate();
+
+  function onBack(e) {
+    e.preventDefault();
+    navigate(-1);
+  }
+
   return (
     <main className="notfound">
       <h1 className="notfound__title">404</h1>
       <p className="notfound__text">{ERROR_MSG_PAGE_NOT_FOUND}</p>
-      <Link
-        to={PAGES.MAIN}
-        className="notfound__link"
-      >
+      <Link onClick={onBack} to={PAGES.MAIN} className="notfound__link">
         Назад
       </Link>
     </main>
